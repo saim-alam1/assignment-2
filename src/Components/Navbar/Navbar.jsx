60a735d9e990ccb5b4e79feaa3272fa650e8b004
saim-alam1 <allbehassansaim@gmail.com>
2026-03-04 23:14:11 +0600

    Adjusted NavBar Max-Width

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,80 +2,88 @@ import plusImg from "../../assets/plus.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+    <div className="bg-base-100 shadow-sm">
+      <div className="max-w-400 mx-auto">
+        <div className="navbar">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex="-1"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>FAQ</a>
+                </li>
+                <li>
+                  <a>Changelog</a>
+                </li>
+                <li>
+                  <a>Blog</a>
+                </li>
+                <li>
+                  <a>Download</a>
+                </li>
+                <li>
+                  <a>Contact</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn border-none shadow-none text-xl md:text-2xl font-bold text-[#130B2D]">
+              CS — Ticket System
+            </a>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>FAQ</a>
-            </li>
-            <li>
-              <a>Changelog</a>
-            </li>
-            <li>
-              <a>Blog</a>
-            </li>
-            <li>
-              <a>Download</a>
-            </li>
-            <li>
-              <a>Contact</a>
-            </li>
-          </ul>
+          <div className="navbar-center"></div>
+          <div className="navbar-end md:mr-1.5">
+            <ul className="menu menu-horizontal hidden lg:flex px-1">
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+              <li>
+                <a>Changelog</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
+              <li>
+                <a>Download</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+            <button className="btn bg-linear-to-br from-[#6832e4] to-[#995cf0] border-none text-white hover:from-[#7a46ee] hover:to-[#a66bff] transition duration-200">
+              <img src={plusImg} alt="Plus Image" />
+              New Ticket
+            </button>
+          </div>
         </div>
-        <a className="btn border-none shadow-none text-xl md:text-2xl font-bold text-[#130B2D]">
-          CS — Ticket System
-        </a>
-      </div>
-      <div className="navbar-center"></div>
-      <div className="navbar-end md:mr-1.5">
-        <ul className="menu menu-horizontal hidden lg:flex px-1">
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>FAQ</a>
-          </li>
-          <li>
-            <a>Changelog</a>
-          </li>
-          <li>
-            <a>Blog</a>
-          </li>
-          <li>
-            <a>Download</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-        </ul>
-        <button className="btn bg-linear-to-br from-[#6832e4] to-[#995cf0] border-none text-white hover:from-[#7a46ee] hover:to-[#a66bff] transition duration-200">
-          <img src={plusImg} alt="Plus Image" />
-          New Ticket
-        </button>
       </div>
     </div>
   );
